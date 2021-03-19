b3f9b7561aa45b41e483b4aa3115a902e8c67f64
Jony Reis <jonyreiscardoso@gmail.com>
2021-03-19 10:39:41 -0400

    add styles on TabNavigator

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,19 +1,63 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
 
 import Home from '../page/Home';
+import Repos from '../page/Repos';
+import Seguidores from '../page/Seguidores';
+import Seguindo from '../page/Seguindo';
 
 const Tab = createBottomTabNavigator();
 
 const AppRoutes = () => (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            switch (route.name) {
+              case 'Home':
+                iconName = 'home';
+                break;
+              case 'Repos':
+                iconName = 'github';
+                break;
+              case 'Seguidores':
+                iconName = 'users';
+                break;
+              case 'Seguindo':
+                iconName = 'users';
+                break;
+              default:
+                iconName = 'circle';
+                break;
+            }
+
+            return <Feather name={iconName} size={size} color={color} />;
+
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#000',
+          inactiveTintColor: '#A5A5A5',
+          iconStyle: { marginTop: '12px' },
+          labelStyle: { fontSize: '14px', marginBottom: '16px' },
+          style: {
+            borderTopLeftRadius: '16px',
+            borderTopRightRadius: '16px',
+            height: '80px',
+            position: "absolute",
+            bottom: 0
+          }
+        }}
+    >
         <Tab.Screen name="Home" component={Home} />
-        {/* <Tab.Screen name="Repos" component={Repos} /> */}
-        {/* <Tab.Screen name="Seguidores" component={Seguidores} /> */}
-        {/* <Tab.Screen name="Seguindo" component={Seguindo} /> */}
-      </Tab.Navigator>
+        <Tab.Screen name="Repos" component={Repos} />
+        <Tab.Screen name="Seguidores" component={Seguidores} />
+        <Tab.Screen name="Seguindo" component={Seguindo} />
+        </Tab.Navigator>
     </NavigationContainer>
 
 );
